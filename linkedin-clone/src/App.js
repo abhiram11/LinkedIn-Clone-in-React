ff9716b556a7c602254c2b3d601b8acd5e5c16c3
abhiram11 <abhiramsatpute@gmail.com>
2021-07-02 14:47:36 +0530

    sidebar complete! added bg, profile, rounded borders, hastags with function/not reusable components, etc

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -9,6 +9,7 @@ import Widgets from "./Widgets";
 //00:35:00 using props to make reusable components
 //00:43:00 justify-content:space-evenly will push the content in the middle
 //00:44:00 stick the header on top. position:sticky, top:0, the z-index: 999 is the vertical STACKING order of elements!
+//1:09:21 using FUNCTIONS instead of REUSABLE COMPONENTS .. !!! Takes in arguements, and returns values!
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
       {/* <h1>Let's build a LinkedIn Clone!</h1> */}
       {/* Header */}
 
-      <Header/>
+      <Header />
 
       {/* App Body  */}
       <div className="app__body">
